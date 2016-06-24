bdc71c002d66043cac3eee077da2c8a42c1fe8c8
Toni <toni.edward@outlook.com>
2016-06-25 00:55:52 +0100

    add test

--- test/DotNetEnv.Tests/Env.cs
+++ test/DotNetEnv.Tests/Env.cs
@@ -1,4 +1,16 @@
-﻿namespace DotNetEnv.Tests
+﻿using System;
+using Xunit;
+
+namespace DotNetEnv.Tests
 {
-    public class Env { }
+    public class Env
+    {
+        [Fact]
+        public void LoadTest()
+        {
+            DotNetEnv.Env.Load();
+            Assert.Equal(Environment.GetEnvironmentVariable("NAME"), "Toni");
+            Assert.Equal(Environment.GetEnvironmentVariable("URL"), "https://github.com/tsolarin");
+        }
+    }
 }
