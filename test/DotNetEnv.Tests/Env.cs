74954759fa51c48908f41b87c87c4a4c8670fad4
Toni <toni.edward@outlook.com>
2016-06-25 01:38:56 +0100

    add test

--- test/DotNetEnv.Tests/Env.cs
+++ test/DotNetEnv.Tests/Env.cs
@@ -12,5 +12,13 @@ namespace DotNetEnv.Tests
             Assert.Equal(Environment.GetEnvironmentVariable("NAME"), "Toni");
             Assert.Equal(Environment.GetEnvironmentVariable("URL"), "https://github.com/tsolarin");
         }
+
+        [Fact]
+        public void LoadPathTest()
+        {
+            DotNetEnv.Env.Load("./.env2");
+            Assert.Equal(Environment.GetEnvironmentVariable("IP"), "127.0.0.1");
+            Assert.Equal(Environment.GetEnvironmentVariable("PORT"), "8080");
+        }
     }
 }
