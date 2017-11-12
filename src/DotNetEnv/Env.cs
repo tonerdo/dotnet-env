4ebb1c5df368a17408b7b17f0d5eabaaa9c6b9e2
Chris Rogus <rogusdev@gmail.com>
2017-11-12 08:43:22 -0500

    rename whitespace in value param, add allow hash in value param, expose and add tests

--- src/DotNetEnv/Env.cs
+++ src/DotNetEnv/Env.cs
@@ -4,13 +4,13 @@ namespace DotNetEnv
 {
     public class Env
     {
-        public static void Load(string path)
+        public static void Load(string path, bool trimWhitespace = true, bool isEmbeddedHashComment = true)
         {
-            Vars envFile = Parser.Parse(File.ReadAllLines(path));
+            Vars envFile = Parser.Parse(File.ReadAllLines(path), trimWhitespace, isEmbeddedHashComment);
             LoadVars.SetEnvironmentVariables(envFile);
         }
 
-        public static void Load()
-            => Load(Path.Combine(Directory.GetCurrentDirectory(), ".env"));
+        public static void Load(bool trimWhitespace = true, bool isEmbeddedHashComment = true)
+            => Load(Path.Combine(Directory.GetCurrentDirectory(), ".env"), trimWhitespace, isEmbeddedHashComment);
     }
 }
--- test/DotNetEnv.Tests/EnvTests.cs
+++ test/DotNetEnv.Tests/EnvTests.cs
@@ -12,6 +12,7 @@ namespace DotNetEnv.Tests
             Assert.Equal(Environment.GetEnvironmentVariable("NAME"), "Toni");
             Assert.Equal(Environment.GetEnvironmentVariable("URL"), "https://github.com/tonerdo");
             Assert.Equal(Environment.GetEnvironmentVariable("CONNECTION"), "user=test;password=secret");
+            Assert.Equal(Environment.GetEnvironmentVariable("WHITEBOTH"), "leading and trailing white space");
         }
 
         [Fact]
@@ -21,6 +22,35 @@ namespace DotNetEnv.Tests
             Assert.Equal(Environment.GetEnvironmentVariable("IP"), "127.0.0.1");
             Assert.Equal(Environment.GetEnvironmentVariable("PORT"), "8080");
             Assert.Equal(Environment.GetEnvironmentVariable("DOMAIN"), "example.com");
+            Assert.Equal(Environment.GetEnvironmentVariable("EMBEDEXPORT"), "some text export other text");
+            Assert.Equal(Environment.GetEnvironmentVariable("COMMENTLEAD"), null);
+            Assert.Equal(Environment.GetEnvironmentVariable("WHITELEAD"), "leading white space followed by comment");
+        }
+
+        [Fact]
+        public void LoadArgsTest()
+        {
+            DotNetEnv.Env.Load(true);
+            Assert.Equal(Environment.GetEnvironmentVariable("WHITEBOTH"), "leading and trailing white space");
+            DotNetEnv.Env.Load(false);
+            Assert.Equal(Environment.GetEnvironmentVariable("  WHITEBOTH  "), "  leading and trailing white space   ");
+            DotNetEnv.Env.Load(true, true);
+            Assert.Equal(Environment.GetEnvironmentVariable("PASSWORD"), "Google");
+            DotNetEnv.Env.Load(true, false);
+            Assert.Equal(Environment.GetEnvironmentVariable("PASSWORD"), "Google#Facebook");
+        }
+
+        [Fact]
+        public void LoadPathArgsTest()
+        {
+            DotNetEnv.Env.Load("./.env2", true, true);
+            Assert.Equal(Environment.GetEnvironmentVariable("WHITELEAD"), "leading white space followed by comment");
+            DotNetEnv.Env.Load("./.env2", false, true);
+            Assert.Equal(Environment.GetEnvironmentVariable("WHITELEAD"), "  leading white space followed by comment  ");
+            DotNetEnv.Env.Load("./.env2", true, false);
+            Assert.Equal(Environment.GetEnvironmentVariable("WHITELEAD"), "leading white space followed by comment  # comment");
+            DotNetEnv.Env.Load("./.env2", false, false);
+            Assert.Equal(Environment.GetEnvironmentVariable("WHITELEAD"), "  leading white space followed by comment  # comment");
         }
     }
 }
