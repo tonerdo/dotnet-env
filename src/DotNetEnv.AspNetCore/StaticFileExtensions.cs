6e0e57f4865166122653f3fca3efa018ecc4232f
Ikechi Michael <mykehell123@gmail.com>
2017-11-22 13:31:05 +0100

    feat: add UseDotNetEnv to IApplicationBuilder for AspNetCore

---
+++ src/DotNetEnv.AspNetCore/StaticFileExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using DotNetEnv;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class StaticFileExtensions
+    {
+        public static IApplicationBuilder UseDotNetEnv(this IApplicationBuilder app) 
+        {
+            Env.Load();
+            return app;
+        }
+
+        public static IApplicationBuilder UseDotNetEnv(this IApplicationBuilder app, string path) 
+        {
+            Env.Load(path);
+            return app;
+        }
+    }
+}
