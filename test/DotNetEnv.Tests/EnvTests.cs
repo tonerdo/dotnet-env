d7ac9be64597220f3c34b62e844ba1659c7e2cc6
Atif Aziz <code@raboof.com>
2018-03-22 15:01:26 +0100

    Clobber using platform case-sensitivity rule
    On Windows, environment names are case-insensitive whereas on Unix-like
    systems they are case-sensitive. When clobbering, use the rules of the
    platform to determine if an environment variable should be clobbered or
    not.

--- test/DotNetEnv.Tests/EnvTests.cs
+++ test/DotNetEnv.Tests/EnvTests.cs
@@ -1,10 +1,51 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
 using Xunit;
 
 namespace DotNetEnv.Tests
 {
-    public class EnvTests
+    public class EnvTests : IDisposable
     {
+        readonly IDictionary<string, string> _oldEnvironment;
+
+        static bool IsWindows => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+        /// <summary>
+        /// Saves the environment before each test is run.
+        /// </summary>
+
+        public EnvTests() =>
+            _oldEnvironment =
+                Environment.GetEnvironmentVariables()
+                           .Cast<DictionaryEntry>()
+                           .ToDictionary(e => e.Key.ToString(),
+                                         e => e.Value.ToString());
+
+        /// <summary>
+        /// Resets the environment after each test is run.
+        /// </summary>
+
+        public void Dispose()
+        {
+            foreach (var var in _oldEnvironment)
+                Environment.SetEnvironmentVariable(var.Key, var.Value);
+
+            var comparer = IsWindows
+                         ? StringComparer.OrdinalIgnoreCase
+                         : StringComparer.Ordinal;
+
+            var newNames = Environment.GetEnvironmentVariables().Keys
+                                      .Cast<string>()
+                                      .Except(_oldEnvironment.Keys,
+                                              comparer);
+
+            foreach (var name in newNames)
+                Environment.SetEnvironmentVariable(name, null);
+        }
+
         [Fact]
         public void LoadTest()
         {
@@ -63,17 +104,22 @@ namespace DotNetEnv.Tests
             Assert.Equal(Environment.GetEnvironmentVariable("UNICODE"), "'\\u00ae \\U0001F680 日本'");
         }
 
-        [Fact]
-        public void LoadNoClobberTest()
+        [Theory]
+        [InlineData("URL", "URL")]
+        [InlineData("url", "URL")]
+        public void LoadNoClobberTest(string setName, string getName)
         {
-            var expected = "totally the original value";
-            Environment.SetEnvironmentVariable("URL", expected);
-            DotNetEnv.Env.Load(false, false, false, false);
-            Assert.Equal(Environment.GetEnvironmentVariable("URL"), expected);
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                setName = getName;
 
-            Environment.SetEnvironmentVariable("URL", "i'm going to be overwritten");
+            var expected = "totally the original value";
+            Environment.SetEnvironmentVariable(setName, expected);
+            DotNetEnv.Env.Load(false, false, false, false);
+            Assert.Equal(Environment.GetEnvironmentVariable(getName), expected);
+
+            Environment.SetEnvironmentVariable(setName, "i'm going to be overwritten");
             DotNetEnv.Env.Load(false, false, false, true);
-            Assert.Equal(Environment.GetEnvironmentVariable("URL"), "https://github.com/tonerdo");
+            Assert.Equal(Environment.GetEnvironmentVariable(getName), "https://github.com/tonerdo");
         }
     }
 }
