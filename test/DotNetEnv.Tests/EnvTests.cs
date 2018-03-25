ff86680c0caff544125ba76420f8bb4590bc65d6
Chris Rogus <rogusdev@gmail.com>
2018-03-25 16:46:33 -0400

    Simplify case-sensitivity tests for env var names
    This removes the need for the test set-up and teardown.

--- test/DotNetEnv.Tests/EnvTests.cs
+++ test/DotNetEnv.Tests/EnvTests.cs
@@ -1,50 +1,12 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
 using Xunit;
 
 namespace DotNetEnv.Tests
 {
-    public class EnvTests : IDisposable
+    public class EnvTests
     {
-        readonly IDictionary<string, string> _oldEnvironment;
-
-        static bool IsWindows => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
-
-        /// <summary>
-        /// Saves the environment before each test is run.
-        /// </summary>
-
-        public EnvTests() =>
-            _oldEnvironment =
-                Environment.GetEnvironmentVariables()
-                           .Cast<DictionaryEntry>()
-                           .ToDictionary(e => e.Key.ToString(),
-                                         e => e.Value.ToString());
-
-        /// <summary>
-        /// Resets the environment after each test is run.
-        /// </summary>
-
-        public void Dispose()
-        {
-            foreach (var var in _oldEnvironment)
-                Environment.SetEnvironmentVariable(var.Key, var.Value);
-
-            var comparer = IsWindows
-                         ? StringComparer.OrdinalIgnoreCase
-                         : StringComparer.Ordinal;
-
-            var newNames = Environment.GetEnvironmentVariables().Keys
-                                      .Cast<string>()
-                                      .Except(_oldEnvironment.Keys,
-                                              comparer);
-
-            foreach (var name in newNames)
-                Environment.SetEnvironmentVariable(name, null);
-        }
+        private static bool IsWindows => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
 
         [Fact]
         public void LoadTest()
@@ -104,22 +66,40 @@ namespace DotNetEnv.Tests
             Assert.Equal(Environment.GetEnvironmentVariable("UNICODE"), "'\\u00ae \\U0001F680 日本'");
         }
 
-        [Theory]
-        [InlineData("URL", "URL")]
-        [InlineData("url", "URL")]
-        public void LoadNoClobberTest(string setName, string getName)
+        [Fact]
+        public void LoadNoClobberTest()
         {
-            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                setName = getName;
-
             var expected = "totally the original value";
-            Environment.SetEnvironmentVariable(setName, expected);
+            Environment.SetEnvironmentVariable("URL", expected);
             DotNetEnv.Env.Load(false, false, false, false);
-            Assert.Equal(Environment.GetEnvironmentVariable(getName), expected);
+            Assert.Equal(Environment.GetEnvironmentVariable("URL"), expected);
 
-            Environment.SetEnvironmentVariable(setName, "i'm going to be overwritten");
+            Environment.SetEnvironmentVariable("URL", "i'm going to be overwritten");
             DotNetEnv.Env.Load(false, false, false, true);
-            Assert.Equal(Environment.GetEnvironmentVariable(getName), "https://github.com/tonerdo");
+            Assert.Equal(Environment.GetEnvironmentVariable("URL"), "https://github.com/tonerdo");
+        }
+
+        [Fact]
+        public void LoadOsCasingTest()
+        {
+            Environment.SetEnvironmentVariable("CASING", "neither");
+            DotNetEnv.Env.Load("./.env3", clobberExistingVars: false);
+            Assert.Equal(Environment.GetEnvironmentVariable("casing"), IsWindows ? "neither" : "lower");
+            Assert.Equal(Environment.GetEnvironmentVariable("CASING"), "neither");
+
+            DotNetEnv.Env.Load("./.env3", clobberExistingVars: true);
+            Assert.Equal(Environment.GetEnvironmentVariable("casing"), "lower");
+            Assert.Equal(Environment.GetEnvironmentVariable("CASING"), IsWindows ? "lower" : "neither");
+
+            Environment.SetEnvironmentVariable("CASING", null);
+            Environment.SetEnvironmentVariable("casing", "neither");
+            DotNetEnv.Env.Load("./.env3", clobberExistingVars: false);
+            Assert.Equal(Environment.GetEnvironmentVariable("casing"), "neither");
+            Assert.Equal(Environment.GetEnvironmentVariable("CASING"), IsWindows ? "neither" : null);
+
+            DotNetEnv.Env.Load("./.env3", clobberExistingVars: true);
+            Assert.Equal(Environment.GetEnvironmentVariable("casing"), "lower");
+            Assert.Equal(Environment.GetEnvironmentVariable("CASING"), IsWindows ? "lower" : null);
         }
     }
 }
