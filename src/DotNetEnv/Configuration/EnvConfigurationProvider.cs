6e9e29783d0f00635ffc45aa3b2fbd1ed07a6fb5
Christian Droulers <droulers.christian@gmail.com>
2023-01-20 22:26:16 -0500

    Add ConfigurationProvider to integrate with standard .NET Configuration providers!

---
+++ src/DotNetEnv/Configuration/EnvConfigurationProvider.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+namespace DotNetEnv.Configuration
+{
+    public class EnvConfigurationProvider : ConfigurationProvider
+    {
+        private readonly string[] paths;
+
+        private readonly LoadOptions options;
+
+        public EnvConfigurationProvider(
+            string[] paths,
+            LoadOptions options)
+        {
+            this.paths = paths;
+            this.options = (options ?? LoadOptions.DEFAULT).NoEnvVars();
+        }
+
+        public override void Load()
+        {
+            IEnumerable<KeyValuePair<string, string>> values;
+            if (this.paths == null)
+            {
+                values = Env.Load(options: this.options);
+            }
+            else
+            {
+                if (this.paths.Length == 1)
+                {
+                    values = Env.Load(this.paths[0], this.options);
+                }
+                else
+                {
+                    values = Env.LoadMulti(this.paths, this.options);
+                }
+            }
+
+            // Since the Load method does not take car of cloberring, We have to check it here!
+            foreach (var value in values)
+            {
+                if (this.options.ClobberExistingVars)
+                {
+                    this.Data[value.Key] = value.Value;
+                }
+                else
+                {
+                    if (!this.Data.ContainsKey(value.Key))
+                    {
+                        this.Data.Add(value.Key, value.Value);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ test/DotNetEnv.Tests/EnvConfigurationTests.cs
@@ -0,0 +1,70 @@
+﻿using DotNetEnv.Configuration;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace DotNetEnv.Tests
+{
+    /// <summary>
+    /// This class tests a few basic operations, but the settings are mostly passed directly
+    /// to `DotNetEnv.Env.Load()`
+    /// </summary>
+    public class EnvConfigurationTests
+    {
+        [Fact]
+        public void AddSourceToBuilderAndLoad()
+        {
+            var config = new ConfigurationBuilder()
+                .AddDotNetEnv()
+                .Build();
+
+            Assert.Empty(config["EMPTY"]);
+            Assert.Equal("'", config["QUOTE"]);
+            Assert.Equal("https://github.com/tonerdo", config["URL"]);
+            Assert.Equal("user=test;password=secret", config["CONNECTION"]);
+            Assert.Equal("  leading and trailing white space   ", config["WHITEBOTH"]);
+            Assert.Equal("SPECIAL STUFF---\nLONG-BASE64\\ignore\"slash", config["SSL_CERT"]);
+        }
+
+        [Fact]
+        public void AddSourceToBuilderAndLoadDotenvHigherSkip()
+        {
+            // ./DotNetEnv.Tests/bin/Debug/netcoreapp3.1/DotNetEnv.Tests.dll -- get to the ./ (root of `test` folder)
+            var config = new ConfigurationBuilder()
+                .AddDotNetEnv("../../../../")
+                .Build();
+
+            Assert.Null(config["NAME"]);
+            Assert.Equal("here", config["TEST"]);
+        }
+
+        [Fact]
+        public void AddSourceToBuilderAndLoadMulti()
+        {
+            var config = new ConfigurationBuilder()
+                .AddDotNetEnvMulti(new[] { "./.env", "./.env2" })
+                .Build();
+
+            Assert.Equal("Other", config["NAME"]);
+        }
+
+        [Fact]
+        public void AddSourceToBuilderAndLoadMultiWithNoClobber()
+        {
+            var config = new ConfigurationBuilder()
+                .AddDotNetEnvMulti(new[] { "./.env", "./.env2" }, LoadOptions.NoClobber())
+                .Build();
+
+            Assert.Equal("Toni", config["NAME"]);
+        }
+
+        [Fact]
+        public void AddSourceToBuilderAndFileDoesNotExist()
+        {
+            var config = new ConfigurationBuilder()
+                .AddDotNetEnv("./.env_DNE")
+                .Build();
+
+            Assert.Empty(config.AsEnumerable());
+        }
+    }
+}
