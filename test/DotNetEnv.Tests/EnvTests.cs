5ab969bab60128dfe0d69ca1c4d024140b3b21d9
Philipp Binder <philipp@kp-binder.de>
2023-12-18 15:41:52 +0100

    added an additional test to show, that by interpolating you can get leading and trailing whitespaces within unquoted values now (before the Trim prevented that)

--- test/DotNetEnv.Tests/EnvTests.cs
+++ test/DotNetEnv.Tests/EnvTests.cs
@@ -177,7 +177,9 @@ namespace DotNetEnv.Tests
             Assert.Equal("test:testtest1 $$ '\" ® and test1", Environment.GetEnvironmentVariable("TEST5_DOUBLE"));
             Assert.Equal("$TEST:$TEST4 \\$\\$ \" \\uae and $TEST1", Environment.GetEnvironmentVariable("TEST5_SINGLE"));
             Assert.Equal("test:testtest1\\uaeandtest1", Environment.GetEnvironmentVariable("TEST5_UNQUOTED"));
-
+            
+            Assert.Equal(" surrounded by spaces ", Environment.GetEnvironmentVariable("TEST_UNQUOTED_WITH_INTERPOLATED_SURROUNDING_SPACES"));
+            
             Assert.Equal("value1", System.Environment.GetEnvironmentVariable("FIRST_KEY"));
             Assert.Equal("value2andvalue1", System.Environment.GetEnvironmentVariable("SECOND_KEY"));
             // EXISTING_ENVIRONMENT_VARIABLE already set to "value"
