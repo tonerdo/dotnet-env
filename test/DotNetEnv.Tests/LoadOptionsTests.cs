04a6aa147f490b15258d2748007e587b66d31046
Philipp Binder <philipp@kp-binder.de>
2024-01-08 16:37:06 +0100

    add tests for objectInitializer and Constructor for LoadOptions

--- test/DotNetEnv.Tests/LoadOptionsTests.cs
+++ test/DotNetEnv.Tests/LoadOptionsTests.cs
@@ -47,6 +47,53 @@ namespace DotNetEnv.Tests
         }
 
         [Fact]
+        public void ConstructorTest()
+        {
+            LoadOptions options;
+
+            options = new LoadOptions();
+            Assert.True(options.SetEnvVars);
+            Assert.True(options.ClobberExistingVars);
+            Assert.True(options.OnlyExactPath);
+
+            options = new LoadOptions(setEnvVars: false);
+            Assert.False(options.SetEnvVars);
+            Assert.True(options.ClobberExistingVars);
+            Assert.True(options.OnlyExactPath);
+
+            options = new LoadOptions(clobberExistingVars: false);
+            Assert.True(options.SetEnvVars);
+            Assert.False(options.ClobberExistingVars);
+            Assert.True(options.OnlyExactPath);
+
+            options = new LoadOptions(onlyExactPath: false);
+            Assert.True(options.SetEnvVars);
+            Assert.True(options.ClobberExistingVars);
+            Assert.False(options.OnlyExactPath);
+        }
+
+        [Fact]
+        public void ObjectInitializerTest()
+        {
+            LoadOptions options;
+
+            options = new LoadOptions() { SetEnvVars = false };
+            Assert.False(options.SetEnvVars);
+            Assert.True(options.ClobberExistingVars);
+            Assert.True(options.OnlyExactPath);
+
+            options = new LoadOptions() { ClobberExistingVars = false };
+            Assert.True(options.SetEnvVars);
+            Assert.False(options.ClobberExistingVars);
+            Assert.True(options.OnlyExactPath);
+
+            options = new LoadOptions() { OnlyExactPath = false };
+            Assert.True(options.SetEnvVars);
+            Assert.True(options.ClobberExistingVars);
+            Assert.False(options.OnlyExactPath);
+        }
+
+        [Fact]
         public void InstanceTest()
         {
             LoadOptions options;
